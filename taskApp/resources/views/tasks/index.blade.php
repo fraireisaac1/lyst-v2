62ab54a9ed18886bd644596c08fa89e3b6691717
ifrair729 <142362748+ifrair729@users.noreply.github.com>
2025-02-18 23:50:54 +0000

    first commit

---
+++ taskApp/resources/views/tasks/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+
+@section('content')
+	<h2 class="text-2xl font-semibold">All Tasks</h2>
+	<!-- Content for displaying tasks goes here -->
+@endsection
