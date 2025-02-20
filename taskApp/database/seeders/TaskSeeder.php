1baa65a7a676e0f674aa0a50f7fd3e9bf0ad3dfa
ifrair729 <142362748+ifrair729@users.noreply.github.com>
2025-02-20 23:34:49 +0000

    third commit

--- taskApp/database/seeders/TaskSeeder.php
+++ taskApp/database/seeders/TaskSeeder.php
@@ -22,5 +22,41 @@ class TaskSeeder extends Seeder
             'priority' => 1, // (1 => low, 2 => medium, 3 => high)
             'category' => 'chores'
         ]);
+        Task::create([
+            'task_name' => 'Washing Dishes',
+            'task_location' => 'Kithen',
+            'time_complexity' => 1, // (1 => ~10 min, 2 => ~30 min, etc.)
+            'materials_required' => 'Soap, Sponge',
+            'deadline' => '2025-02-20 11:00:00',
+            'priority' => 2, // (1 => low, 2 => medium, 3 => high)
+            'category' => 'chores'
+        ]);
+        Task::create([
+            'task_name' => 'Mowing the Lawn',
+            'task_location' => 'Front Yard',
+            'time_complexity' => 2, // (1 => ~10 min, 2 => ~30 min, etc.)
+            'materials_required' => 'Lawn Mower, Trash Bags',
+            'deadline' => '2025-02-20 14:30:00',
+            'priority' => 2, // (1 => low, 2 => medium, 3 => high)
+            'category' => 'chores'
+        ]);
+        Task::create([
+            'task_name' => 'Cleaning your Room',
+            'task_location' => 'Your Bedroom',
+            'time_complexity' => 3, // (1 => ~10 min, 2 => ~30 min, etc.)
+            'materials_required' => 'Trash Bags, Boxes, Hamper',
+            'deadline' => '2025-02-20 12:30:00',
+            'priority' => 3, // (1 => low, 2 => medium, 3 => high)
+            'category' => 'chores'
+        ]);
+        Task::create([
+            'task_name' => 'Getting Coffe for your Boss',
+            'task_location' => 'At your Office',
+            'time_complexity' => 1, // (1 => ~10 min, 2 => ~30 min, etc.)
+            'materials_required' => 'Coffe, Water, Coffe Machine',
+            'deadline' => '2025-02-20 07:30:00',
+            'priority' => 3, // (1 => low, 2 => medium, 3 => high)
+            'category' => 'work'
+        ]);
     }
 }
