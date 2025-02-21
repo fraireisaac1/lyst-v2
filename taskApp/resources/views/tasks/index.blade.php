4914db24daa4c733a419eb4b02f7c52f30725a37
christianBarrAkaMonkey <142362628+christianBarrAkaMonkey@users.noreply.github.com>
2025-02-21 00:16:43 +0000

    edit.blade.php

--- taskApp/resources/views/tasks/index.blade.php
+++ taskApp/resources/views/tasks/index.blade.php
@@ -24,6 +24,7 @@
 			<option value="category" {{request('sort') === 'category' ? 'selected' : ''}}>
 				category
 			</option>
+
 		</select>
 
 		<button type="submit" class="bg-blue-500 text-white px-4 py-2 ml-2 rounded hover:bg-blue-600">
